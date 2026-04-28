27f4224c10c2be09f23685cd1ee9b0ba7e7bad73
DIAZ <ferdiazprasida@gmail.com>
2026-04-28 13:41:52 +0700

    feat: configure project-wide build directories and dynamic namespace assignment in build.gradle.kts

--- mobile/android/build.gradle.kts
+++ mobile/android/build.gradle.kts
@@ -16,6 +16,25 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
+    afterEvaluate {
+        val androidExt = extensions.findByName("android")
+        if (androidExt != null) {
+            try {
+                val namespaceMethod = androidExt.javaClass.getMethod("getNamespace")
+                val currentNamespace = namespaceMethod.invoke(androidExt)
+                if (currentNamespace == null) {
+                    val groupStr = project.group.toString()
+                    val setNamespaceMethod = androidExt.javaClass.getMethod("setNamespace", String::class.java)
+                    setNamespaceMethod.invoke(androidExt, groupStr)
+                }
+            } catch (e: Exception) {
+                // Ignore if methods don't exist
+            }
+        }
+    }
+}
+
+subprojects {
     project.evaluationDependsOn(":app")
 }
 
